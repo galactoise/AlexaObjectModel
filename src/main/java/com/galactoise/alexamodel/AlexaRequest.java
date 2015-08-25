02e84c00862ea8a7bbef2ec66b67b9480f59ebb0
Eric <galactoise@gmail.com>
2015-08-24 19:36:52 -0700

    Added new fields to Alexa Requests

--- src/main/java/com/galactoise/alexamodel/AlexaRequest.java
+++ src/main/java/com/galactoise/alexamodel/AlexaRequest.java
@@ -5,30 +5,53 @@ public class AlexaRequest {
 	private AlexaRequestIntent intent;
 	private AlexaRequestType type;
 	private String requestId;
+	private String timestamp;
+	private String reason;
 	
 	public AlexaRequestIntent getIntent() {
 		return intent;
 	}
+	
 	public void setIntent(AlexaRequestIntent intent) {
 		this.intent = intent;
 	}
+	
 	public AlexaRequestType getType() {
 		return type;
 	}
+	
 	public void setType(AlexaRequestType type) {
 		this.type = type;
 	}
+	
 	public String getRequestId() {
 		return requestId;
 	}
+	
 	public void setRequestId(String requestId) {
 		this.requestId = requestId;
 	}
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
 	@Override
 	public String toString() {
 		return "AlexaRequest [intent=" + intent + ", type=" + type
-				+ ", requestId=" + requestId + "]";
+				+ ", requestId=" + requestId + ", timestamp=" + timestamp
+				+ ", reason=" + reason + "]";
 	}
-	
-	
 }
--- src/main/java/com/galactoise/alexamodel/AlexaRequestIntent.java
+++ src/main/java/com/galactoise/alexamodel/AlexaRequestIntent.java
@@ -5,23 +5,26 @@ import java.util.HashMap;
 public class AlexaRequestIntent {
 
 	private String name;
-	private HashMap<String, Object> slots;
+	private HashMap<String, AlexaRequestIntentSlotObject> slots;
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-	public HashMap<String, Object> getSlots() {
+	
+	public HashMap<String, AlexaRequestIntentSlotObject> getSlots() {
 		return slots;
 	}
-	public void setSlots(HashMap<String, Object> slots) {
+	
+	public void setSlots(HashMap<String, AlexaRequestIntentSlotObject> slots) {
 		this.slots = slots;
 	}
+	
 	@Override
 	public String toString() {
 		return "AlexaRequestIntent [name=" + name + ", slots=" + slots + "]";
 	}
-	
-	
 }
